16d7c3cef260cad094ae4959647ca87bd2da4b8d
Levent <sanxian.li@qq.com>
2017-11-15 16:27:52 +0800

    Update compact.js

--- compact.js
+++ compact.js
@@ -1,24 +1,27 @@
 /**
- * Creates an array with all falsey values removed. The values `false`, `null`,
- * `0`, `""`, `undefined`, and `NaN` are falsey.
+ * 创建一个新数组并包含原数组中所有的非假值元素。  
+ * 例如 false、null、 0、""、undefined 和 NaN 都是“假值”。
  *
- * @since 0.1.0
- * @category Array
- * @param {Array} array The array to compact.
- * @returns {Array} Returns the new array of filtered values.
+ * @版本 >=0.1.0
+ * @类型 Array
+ * @参数 {Array} 过滤数组对象
+ * @返回值 {Array} 返回过滤假值后的数组.
  * @example
  *
  * compact([0, 1, false, 2, '', 3])
  * // => [1, 2, 3]
  */
 function compact(array) {
+  // 预定义
   let resIndex = 0
   const result = []
-
+  
+  // 参数数组为null 返回空数组 
   if (array == null) {
     return result
   }
 
+  // 遍历数组 如果是 false、null、 0、""、undefined 和 NaN 等值都是 在if语句中 都是 false 只有 true 的才给返回数组插值
   for (const value of array) {
     if (value) {
       result[resIndex++] = value
